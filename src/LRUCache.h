b95feac980c0770207054475e65047474eb1df90
Deaglan Halligan <deaglanhalligan@gmail.com>
2015-04-24 14:35:47 -0700

    Changed comment style for doxygen

--- src/LRUCache.h
+++ src/LRUCache.h
@@ -5,7 +5,9 @@ using std::unordered_map;
 using std::list;
 using std::pair;
 
-// LRUCache implements a simple cache with LRU (least recently used) replacement policy
+/*
+  LRUCache implements a simple cache with LRU (least recently used) replacement policy
+*/
 template <class K, class V, size_t capacity>
 class LRUCache
 {
