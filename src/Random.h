080463f5ef4c1e6d3b43401d28653bb20765039d
Deaglan Halligan <deaglanhalligan@gmail.com>
2015-07-20 16:39:13 -0700

    Added simple docs to Random

--- src/Random.h
+++ src/Random.h
@@ -6,12 +6,16 @@
 using std::default_random_engine;
 using std::uniform_int_distribution;
 
+/*! \brief Rand_int randomly generates an integer in the given range
+ *
+ *  My detailed description
+ */
 class Rand_int 
 {
  public:
   Rand_int(int low, int high) : dist{low, high} {}
   int operator() () { return dist(re); }
- private:
+  private:
   
   default_random_engine re;
   uniform_int_distribution<> dist;
